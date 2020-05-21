c72e4f30cadde826f84f7bd7c4aaebc946de85a6
Bryan Alfaro González <alfabrayan12@gmail.com>
2020-05-20 21:35:26 -0600

    Test

--- Test/src/Tests.java
+++ Test/src/Tests.java
@@ -1,7 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
